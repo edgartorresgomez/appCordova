f1a25a3abd8fc92eb712898bb8e8ec57cfe7cb26
unknown <edgar.torres@decameron.com>
2019-07-10 17:22:50 -0500

    prueba

--- www/js/index.js
+++ www/js/index.js
@@ -1,45 +1,15 @@
-var app = {
 
-    inicio: function(){
-        document.addEventListener("deviceready", this.onDeviceReady.bind(this), false);
-    },
+(function () {
+    
+    document.addEventListener("deviceready", onDeviceReady, false);
 
-    onDeviceReady: function() {
+    function onDeviceReady() {
         // Register the event listener
-        //document.addEventListener("backbutton", onBackKeyDown, false);
-        this.receivedEvent('deviceready');
-        //alert("Aplicacion lista");
-    },
-
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        alert('Received Event: ' + id);
+        document.addEventListener("backbutton", onBackKeyDown, false);
+    };
+    
+    function onBackKeyDown(){
+        alert("bien atras");
     }
 
-
-};
-app.inicio();
-
-/*
-function inicio() {
-    document.addEventListener("deviceready", onDeviceReady, false);
-}
-
-function onDeviceReady() {
-    // Register the event listener
-    document.addEventListener("backbutton", onBackKeyDown, false);
-}
-function onBackKeyDown(){
-    alert("bien atras");
-    
-}
-
-inicio();
-
-*/
+})();
