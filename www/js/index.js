9849c93c70a4211f3bb3b4772e22f3d56ce1d595
unknown <edgar.torres@decameron.com>
2019-07-10 17:02:18 -0500

    prueba de encapsulamiento

--- www/js/index.js
+++ www/js/index.js
@@ -1,13 +1,25 @@
 var app = {
 
     inicio: function(){
-        document.addEventListener("deviceready", onDeviceReady, false);
+        document.addEventListener("deviceready", this.onDeviceReady.bind(this), false);
     },
 
     onDeviceReady: function() {
         // Register the event listener
-        document.addEventListener("backbutton", onBackKeyDown, false);
-        alert("Aplicacion lista");
+        //document.addEventListener("backbutton", onBackKeyDown, false);
+        this.receivedEvent('deviceready');
+        //alert("Aplicacion lista");
+    },
+
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        alert('Received Event: ' + id);
     }
 
 
