98d6b1ed40d92dc57bd3040b166a047d32a443c5
unknown <edgar.torres@decameron.com>
2019-07-11 20:39:50 -0500

    prueba click

--- www/js/index.js
+++ www/js/index.js
@@ -6,11 +6,22 @@
     function onDeviceReady() {
         // Register the event listener
         document.addEventListener("backbutton", onBackKeyDown, false);
+        document.addEventListener("click", funcion1, false);
+        document.addEventListener("click", funcion2, false);
+
+    };
+
+    function funcion1() {
+        alert("funcion1");
+    };
+
+    function funcion2() {
+        alert("funcion2");
     };
     
     function onBackKeyDown(){
         alert("bien atras");
-        window.opener = self;
+        window.navigator.
         window.close();
         alert("bien atras no cerro");
 
