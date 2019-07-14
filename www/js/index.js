4d2f430dceb9d5f92f398f0ca95f32d72c592e1a
unknown <edgar.torres@decameron.com>
2019-07-13 19:44:36 -0500

    btns

--- www/js/index.js
+++ www/js/index.js
@@ -9,15 +9,23 @@
 
         
         var btn1 = document.querySelector('#btn1');
-        btn1.addEventListener("click", funcion1);
+        btn1.addEventListener("click", funcion1,false);
 
         var btn2 = document.querySelector('#btn2');
-        btn2.addEventListener("click", funcion2);
+        btn2.addEventListener("click", funcion2, false);
         //document.addEventListener("click", funcion2, false);
 
+        var btns = document.querySelectorAll('btn')
+
+        for (btn of btns){
+            alert (btns.length);
+            btn.addEventListener('click', fn, false);
+        }
     };
 
-    
+    function fn() {
+        alert('bien');
+    };
 
     function funcion1() {
         alert("funcion1");
